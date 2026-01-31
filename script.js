3755dc58021667245365f7485f981361ad01798f
ArchiePataray <archiepataray27@gmail.com>
2026-01-31 13:35:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,3 @@ onload = () => {
   document.body.classList.remove("container");
 };
 
-   function backToMessage() {
-  // 1. Hide the gift/flower page
-  document.getElementById("giftPage").classList.add("hidden");
-  
-  // 2. Show the letter/message page
-  document.getElementById("messagePage").classList.remove("hidden");
-}
-
-// Ensure your "Open Gift" button also uses the correct ID:
-function showGift() {
-  document.getElementById("messagePage").classList.add("hidden");
-  document.getElementById("giftPage").classList.remove("hidden");
