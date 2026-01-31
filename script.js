744699005e2a8cead78dd0196ed9a231b3f75c04
ArchiePataray <archiepataray27@gmail.com>
2026-01-31 13:24:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ onload = () => {
   document.body.classList.remove("container");
 };
 
+        function backToMessage() {
+  // Hide the flower container (change ID to match your container)
+  document.getElementById("flower-container").classList.add("hidden"); 
+  
+  // Show the message page
+  document.getElementById("messagePage").classList.remove("hidden");
+}
+  
